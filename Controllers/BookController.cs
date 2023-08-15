9c2c21c98b4b30d21de77b36af939a85d3f7358e
unknown <paul7markets@hotmail.com>
2023-08-15 14:22:49 +0100

    Book Details displayed

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -16,7 +16,12 @@ public class BookController : Controller
     [HttpGet("book")]
     public IActionResult Book()
     {
-        var book = new BookViewModel("8127398173212", "The Lord Of The Rings");
+        var book = new BookViewModel("0261103253", "The Lord Of The Rings","J.R.R Tolkien"){
+            CoverPhotoUrl= "https://m.media-amazon.com/images/P/0261103253.01._SCLZZZZZZZ_SX500_.jpg",
+            Blurb = "In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell by chance into the hands of the hobbit Bilbo Baggins.",
+            Genre = "Fantasy",
+            YearPublished = 1955,
+        };
         return View(book);
     }
 }
--- Models/BookViewModel.cs
+++ Models/BookViewModel.cs
@@ -4,13 +4,15 @@ public class BookViewModel
 {
     public string Isbn { get; }
     public string Title { get; }
+    public string Author { get; }
     public string? CoverPhotoUrl { get;  set;}
     public string? Blurb { get; set; }
     public string? Genre { get; set;}
     public int? YearPublished { get; set; }
 
-    public BookViewModel(string isbn, string title){
+    public BookViewModel(string isbn, string title, string author){
         Isbn = isbn;
         Title = title;
+        Author = author;
     }
 }
