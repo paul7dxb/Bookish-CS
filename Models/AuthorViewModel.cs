f663d9a46703167b87f59df60322b5a91c435e5f
Will Gerry <william.gerry@outlook.com>
2023-08-16 11:10:09 +0100

    Add author indexing for navigation. Return 404 if not found

--- Models/AuthorViewModel.cs
+++ Models/AuthorViewModel.cs
@@ -2,12 +2,14 @@ namespace Bookish.Models;
 
 public class AuthorViewModel
 {
+    public int AuthorId { get; }
     public string Name { get; }
     public int? BirthYear { get; set;}
     public string? PhotoUrl { get; set;}
     public string? Bio { get;  set;}
 
-    public AuthorViewModel(string name){
+    public AuthorViewModel(int authorId, string name){
+        AuthorId = authorId;
         Name = name;
     }
 }
